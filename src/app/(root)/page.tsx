eb8879414cc6f66ed00037b34e77e2ba7aac6671
Kaori Kawakami <k.kawakami08@gmail.com>
2025-03-13 12:21:55 -0700

    imported sample data and created basic product card

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <ProductList data={sampleData} title="Newest Arrivals" limit={4} />
+    </div>
+  );
 };
 
 export default HomePage;
