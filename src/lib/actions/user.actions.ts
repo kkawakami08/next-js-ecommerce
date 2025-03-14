ec20c3e3840b2436b42922255d95d7a558a10325
Kaori Kawakami <k.kawakami08@gmail.com>
2025-03-14 15:39:41 -0700

    error handling in sign up form

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
+import { formatError } from "../utils";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -67,12 +68,16 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
       message: "User registered successfully",
     };
   } catch (error) {
+    // console.log(error.name);
+    // console.log(error.code);
+    // console.log(error.errors);
+    // console.log(error.meta?.target);
     if (isRedirectError(error)) {
       throw error;
     }
     return {
       status: false,
-      message: "User was not registered",
+      message: formatError(error),
     };
   }
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,3 +18,29 @@ export function formatNumberWithDecimal(num: number): string {
     ? `${intValue}.${floatValue.padEnd(2, "0")}`
     : `${intValue}.00`;
 }
+
+//format errors
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function formatError(error: any) {
+  if (error.name === "ZodError") {
+    //handle Zod Error
+    //get array of messages from error.errors
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
+
+    return fieldErrors.join(". ");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    //handle prisma error
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
+  } else {
+    //handle other errors
+    return typeof error.message === "string"
+      ? error.messsage
+      : JSON.stringify(error.message);
+  }
+}
